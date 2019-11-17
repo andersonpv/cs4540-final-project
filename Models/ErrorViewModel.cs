4c35597d8b203162bef46a5ee367537169df752d
andersonpv <anderson.porta@outlook.com>
2019-11-16 21:10:35 -0700

    set up project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cs4540_final_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

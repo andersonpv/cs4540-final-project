b89e8498486b2ab9dd48ed0057a4b93af88faf8f
Tanner Livingston <tannerrosslivingston@gmail.com>
2019-11-20 16:26:54 -0700

    Re-added two models

---
+++ Models/DaySchedule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cs4540_final_project.Models
+{
+    /* 
+     * Worker availability for a single day
+     */
+    public class DaySchedule
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DayScheduleID { get; set; }
+
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+
+        public bool[] Availability { get; set; }
+
+    }
+}
---
+++ Models/Worker.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cs4540_final_project.Models
+{
+    /*
+ * This class represents the model for a worker (i.e. a Barber, Hair Stylist, etc)
+ */
+    public class Worker
+    {
+        [ForeignKey("UserId")]
+        public IdentityUser User { get; set; }
+
+        // Job title
+        public string Job { get; set; }
+
+        // Worker's name
+        public string Name { get; set; }
+
+        // What services the worker provides (i.e. beard, haircut, etc)
+        public ICollection<string> Services { get; set; }
+
+        public ICollection<DaySchedule> Schedule { get; set; }
+
+       // ICollection<Comment> Reviews { get; set; }
+    }
+}
